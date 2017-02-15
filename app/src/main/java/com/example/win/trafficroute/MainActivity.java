4b31f7ce4be45d482bcd71efb5c28357a7211f26
charasril <charasri2000@yahoo.com>
2017-02-15 11:01:57 +0700

    21070215-#9- backup Create Database

--- app/src/main/java/com/example/win/trafficroute/MainActivity.java
+++ app/src/main/java/com/example/win/trafficroute/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.win.trafficroute;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -38,4 +38,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             finish();
         }
     } //onClick
+
+
 }
--- app/src/main/java/com/example/win/trafficroute/db/DatabaseHelper.java
+++ app/src/main/java/com/example/win/trafficroute/db/DatabaseHelper.java
@@ -16,17 +16,25 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "ITM801MAPRoute.db";
     private static final int DATABASE_VERSION = 1;
 
-    public static final String TABLE_NAME = "SEARCH_USE_ROUTE_LIST";
-    public static final String COL_ID = "_id";
+    public static final String TABLE_ROUTE_NAME = "SEARCH_USE_ROUTE_LIST";
+    public static final String COL_ROUTE_ID = "_id";
     public static final String COL_USERNAME = "USER_NAME";
     public static final String COL_USE_DATE = "USE_DATETIME";
-    public static final String COL_START = "USER_START";
+    public static final String COL_START_WAYPOINT = "USER_START_WAYPOINT";
     public static final String COL_START_LAT = "USER_LATITUDE_START";
     public static final String COL_START_LNG = "USER_LONGTITUDE_START";
-    public static final String COL_END = "USER_END";
+    public static final String COL_END_WAYPOIN = "USER_END_WAYPOINT";
     public static final String COL_END_LAT = "USER_LATITUDE_END";
     public static final String COL_END_LNG = "USER_LONGTITUDE_END";
 
+    public static final String TABLE_USER_NAME = "TABLE_USER_INFORMATION";
+    public static final String COL_USER_ID = "_id";
+    public static final String COL_USER_NAME = "USER_NAME";
+    public static final String COL_USER_PASSWORD= "USER_PASSWORD";
+    public static final String COL_USER_FNAME = "USER_FNAME";
+    public static final String COL_USER_LNAME = "USER_LNAME";
+    public static final String COL_USER_EMAIL = "USER_EMAIL";
+    public static final String COL_USER_ACTIEVE_DATE = "USER_ACTIVE_DATE";
 
 
     public DatabaseHelper(Context context) {
@@ -35,13 +43,31 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        Log.d(TAG, "Creating table");
+        Log.d(TAG, "Creating table==>"+TABLE_ROUTE_NAME);
 
-//        String SQL_CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + "("
-//                + COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-//                + COL_SCORE + " REAL, "
-//                + COL_DIFFICULTY + " INTEGER" + ")";
-//        db.execSQL(SQL_CREATE_TABLE);
+  String SQL_CREATE_TABLE_ROUTE = "CREATE TABLE " + TABLE_ROUTE_NAME + "("
+                + COL_ROUTE_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + COL_USERNAME + " TEXT, "
+                + COL_USE_DATE + " TEXT, "
+                + COL_START_WAYPOINT + " TEXT, "
+                + COL_START_LAT + " TEXT, "
+                + COL_START_LNG + " TEXT, "
+                + COL_END_WAYPOIN + " TEXT, "
+                + COL_END_LAT + " TEXT, "
+                + COL_END_LNG + " TEXT" + ")";
+        db.execSQL(SQL_CREATE_TABLE_ROUTE);
+
+         String SQL_CREATE_TABLE_USER = "CREATE TABLE " + TABLE_USER_NAME + "("
+                + COL_USER_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + COL_USER_NAME + " TEXT, "
+                + COL_USER_PASSWORD + " TEXT, "
+                + COL_USER_FNAME + " TEXT, "
+                + COL_USER_LNAME + " TEXT, "
+                + COL_USER_EMAIL + " TEXT, "
+                + COL_USER_ACTIEVE_DATE + " DATE" + ")";
+        db.execSQL(SQL_CREATE_TABLE_USER);
+
+
     }
 
     @Override
